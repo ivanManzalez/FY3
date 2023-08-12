ca46d4c9749fd40cc6a613a0530992cf73441229
Ivan Mancia-Gonzalez <65386828+ivanManzalez@users.noreply.github.com>
2023-08-12 15:59:21 -0400

    allow fields to be null (#15)
    * allow fields to be null
    
    * add jsx extensions to be read by webpack

--- seasons/models/season.py
+++ seasons/models/season.py
@@ -6,8 +6,8 @@ from teams.models import Team
 class Season(models.Model):
   season_year = models.PositiveIntegerField(default=timezone.now().year)
   start_date = models.DateTimeField(default=timezone.now())
-  end_date = models.DateTimeField(default=None)
-  champions = models.ForeignKey(Team, default = None, on_delete=models.CASCADE)
+  end_date = models.DateTimeField(default=None, null=True)
+  champions = models.ForeignKey(Team, default = None, null=True, on_delete=models.CASCADE)
   
   def clean(self):
       if self.start_date and self.end_date and self.start_date > self.end_date:
