3fe6f19f1a7d4c926842576c37e66af5ca244ddc
ivanManzalez98 <ivanmanciag@gmail.com>
2023-08-12 12:58:40 -0700

    add jsx extensions to be read by webpack

--- frontend/src/components/navigator/Bar.js
+++ frontend/src/components/navigator/Bar.js
@@ -15,7 +15,7 @@ export default class Bar extends Component{
         <li><Link to="/standings">Standings</Link></li>
         <li><Link to="/stats">Stats</Link></li>
         <li><Link to="/schedule">Schedule</Link></li>
-        <li><a href="http://127.0.0.1:8000/players/players">API</a></li>
+        <li><a href="http://127.0.0.1:8000/players/player">API</a></li>
       </ul>
     </nav>
     );
